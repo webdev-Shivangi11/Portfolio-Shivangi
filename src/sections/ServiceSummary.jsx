b32ac734fb6ce83acefb6a749e938ef12f61199e
officialshivangik11@gmail.com <officialshivangik11@gmail.com>
2025-10-03 12:35:08 +0530

    some error ressolved

--- src/sections/ServiceSummary.jsx
+++ src/sections/ServiceSummary.jsx
@@ -36,7 +36,7 @@ const ServiceSummary = () => {
   return (
     <section className="mt-20 overflow-hidden font-light leading-snug text-center mb-42 contact-text-responsive">
       <div id="title-service-1">
-        <p>Architucture</p>
+        <p>Architecture</p>
       </div>
       <div
         id="title-service-2"
